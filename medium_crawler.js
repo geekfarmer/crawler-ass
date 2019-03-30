ebefc86f92aa94e0e8826fb647f883927801d076
geekfarmer <anuragpatel.optnio@gmail.com>
2019-03-31 05:06:35 +0530

    final code for scrapping required  data

--- medium_crawler.js
+++ medium_crawler.js
@@ -2,49 +2,84 @@ import request from 'request'
 import fs from 'fs'
 import cheerio from 'cheerio'
 import async from 'async'
+import json2csv from 'json2csv'
 
 var root = ''
 var q = null
-var links = []
+var links = {}
 var link = ''
 
-const stripTrailingSlash = (str) => {
-    if (str.substr(-1) === '/') {
+const stripTrailingSlash = (str) => {    
+    if (str.substr(-1) === '/') {       
         return str.substr(0, str.length - 1);
-    }
+    }    
     return str;
+}
+
+const getParamsFromURL = (str) => {
+    if(str){
+        var queryDict = {}
+        var param = []
+        str.substr(0).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]})
+        param = Object.keys(queryDict)
+        return param
+    }
 }
 
 const pageProcessor = (htmlString) => {
 
     let ch = cheerio.load(htmlString);
-    // Get only those links which point to same site
+    // filter links only belongs medium.com
     ch(`a[href^="${root}"]`).each((i, a) => {
         // Ignore query params. Those point to same URL
-        link = stripTrailingSlash(a.attribs.href.split('?')[0]);
-
+        let params = []
+        params = getParamsFromURL(a.attribs.href.split('?')[1])
+        link = stripTrailingSlash(a.attribs.href.split('?')[0])
+        
         // Ignore URLs which are already there
         if (!links[link]) {
-            links[link] = 'In Queue........';
+            links[link] = {};
+            links[link]['status'] = 'false'; 
+            links[link]['count'] = 1;
+            links[link]['params'] = ['']
+
+            if(!params){
+                links[link]['params'] = ['']
+            }
+            else if(params){
+                links[link]['params'] = params;
+            }            
+                    
             q.push(link);
             fs.appendFile('url.csv', link + "\n", function (err, result) {
                 if (err) console.log('error', err);
             });
-        }
+        }else {
+            // increase count value for repeated url;
+            if(!links[link]['params']){
+                links[link]['params'] = ['']
+            }
+            links[link]['count'] += 1;             
+            if(params && params.length > 0){
+                let joinArray = links[link]['params'].concat(params);
+                links[link]['params'] = [... new Set( joinArray)];  
+            }
+        }        
     });
+    console.log(Object.values(links));
+    
 }
 
-export const fetchAndParser = (url, callback) => {
-    console.log(`started ${url}`);
+const fetchAndParser = (url, callback) => {
     request(url, (error, response, body) => {
-        console.log("..................");
         if (error) {
             console.log("Network Error", error);
             callback(error, url);
         } else {
             pageProcessor(body);
-            links[url] = 'completed';
-            console.log('completed', url);
+            links[url] = {};
+            links[url]['status'] = 'true';
+            links[url]['count'] = 1; 
             callback(error, url);
         }
     });
@@ -53,6 +88,8 @@ export const fetchAndParser = (url, callback) => {
 export const requestHandler = (url) => {
     root = url
     q = async.queue(fetchAndParser, 5)
-    links[url] = 'In Queue........';
+    // links[url] = {};
+    // links[url]['status'] = 'false'; 
+    // links[url]['count'] = 1; 
     fs.unlink('url.csv', () => q.push(stripTrailingSlash(root)));
 }
